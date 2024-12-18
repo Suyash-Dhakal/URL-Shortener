1555794b229256b18698df77bac5cb016d410752
Suyash Dhakal <suyash.dhakal@icloud.com>
2024-12-18 20:09:39 +0545

    transition from stateful authentication (Session Based) to Stateless authentication (Token Based) although token initially stored in the form of client side cookie

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const {getUser}=require('../service/auth');
+const User=require('../models/user');
 
 async function restrictToLoggedinUserOnly (req,res,next){
     const userUid=req.cookies?.uid;
@@ -9,17 +10,33 @@ async function restrictToLoggedinUserOnly (req,res,next){
     const user=getUser(userUid);
     if(!user) return res.redirect('/login');
 
+    const dbUser=await User.findById(user._id);
+    if(!dbUser) {
+        return res.redirect('/login'); //in the case where user is not found in the db
+    }
+
     req.user=user;
     next();
-    
 }
 
 async function checkAuth(req,res,next){
     const userUid=req.cookies?.uid;
     const user=getUser(userUid);
     
-    req.user=user;
-    next();
+    if (!user) {
+        req.user = null; // Indicate that the user is not authenticated
+        next();
+      } else {
+        // **Additional validation step:**
+        const dbUser = await User.findById(user._id);
+        if (!dbUser) {
+          req.user = null; // User not found in the database
+          next();
+        } else {
+          req.user = dbUser;
+          next();
+        }
+      }
 }
 
 module.exports={
